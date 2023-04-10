02652d90ab1c3fd9c25362f5193f6fa7508fe060
Dynastiy <emmanuel00michael@gmail.com>
2023-04-10 14:06:58 +0100

    updated privacy policy

--- src/modules/landing/views/PrivacyPolicy.vue
+++ src/modules/landing/views/PrivacyPolicy.vue
@@ -1,18 +1,257 @@
 <template>
-    <div id="privacy-policy" class="container mt-3">
-      <h1 class="header-font">
-          Privacy Policy
-      </h1>
-      <hr>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-  
-  }
-  </script>
-  
-  <style>
-  
-  </style>
+  <div id="privacy-policy" class="container mt-3">
+    <h1 class="header-font">PI2P Privacy Policy</h1>
+    <hr />
+    <!-- <p style="text-align: center"><strong>PI2P PRIVACY POLICY</strong></p> -->
+    <p>
+      <span style="color: #202124">PRIVACY POLICY FOR </span
+      ><strong>PI2P </strong>mobile Application
+    </p>
+    <p>
+      <strong>PI2P</strong
+      ><span style="color: #202124">
+        operates&nbsp; and manages this mobile application.</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >1. Description of parties:&nbsp; For the convenience of wording in this
+        agreement/Privacy policy(the &ldquo;Agreement&rdquo;), this </span
+      ><strong>PI2P</strong
+      ><span style="color: #202124">
+        Platform/Service is referred to as &ldquo;we&rdquo;, &ldquo;us&rdquo; or
+        &ldquo;our''. Users of and other visitors to the Platform are referred
+        to as &ldquo;you&rdquo;, &ldquo;your'' or &ldquo;User&rdquo;. We and you
+        are collectively referred to as &ldquo;both parties' ' and as
+        &ldquo;a/one party&rdquo; individually.&nbsp;</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >2. Purpose: This privacy policy informs you of our policies regarding
+        the collection, use, and disclosure of your personal data when you use
+        our Platform and the choices you have associated with that data.</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >3. Consent: By using the Service, users agree to the collection and use
+        of information in accordance with this Privacy Policy. Unless otherwise
+        defined in this Privacy Policy, terms used in this Privacy Policy have
+        the same meanings as in our Terms of use. Users hereby acknowledge and
+        understand that, at the point of logging into the Platform, the User
+        will be deemed to have expressed to us their acceptance, consent,
+        undertaking and confirmation of the following, regardless of whether the
+        user has completed their registration on the Platform:</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >（a）that the User agrees, on a voluntary basis, to disclose personal
+        information to us;&nbsp;</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >（b）the User will abide by all the terms and conditions of this
+        Agreement;&nbsp;</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >（c）the User agrees and authorizes the Platform to collect the
+        User&rsquo;s personal information when the User registers with the
+        Platform, logs into the Platform, and/or uses the Platform
+        services;&nbsp;</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >（d）the User agrees to all the terms and conditions of this Agreement
+        and agrees to accept any amendment that may subsequently be made to the
+        Privacy Policy;&nbsp;</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >（e）the User agrees to be informed about our products and services, by
+        email or push notification if we think they are or may be of interest to
+        you, (unless the User has indicated that he or she does not wish to
+        receive such information, by unsubscribing to such notifications).</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >4. Scope of Application: This agreement shall apply to this platform's
+        users. Subject to any and all laws relating to the protection of
+        personal information and data in the nation or region where this
+        Platform is based, Users, shall abide by the terms and conditions of
+        this Agreement.</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >5. Severability: If any of the provisions of this agreement are found
+        to be null and void, the remaining provisions of this agreement shall
+        remain valid and shall continue to bind the Parties unless it is clear
+        from the circumstances that, in the absence of the provision (s) found
+        to be null and void, the Parties would not have concluded the present
+        agreement. The Parties, if necessary with the assistance of an Arbitral
+        Tribunal, shall replace all provisions found to be null and void with
+        provisions that are valid under the applicable law</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >and come closest to their original intention.</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >6. References: All references to this agreement shall be deemed as
+        references to the most recent version thereof, regardless of whether any
+        of such amendment is made before or after the signing of this
+        Agreement.</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >7. Information Collected: We collect Personal Data from you, this
+        includes but is not limited to &ndash; Your name, email address, and
+        your password, phone number. This information helps us address customer
+        support issues, improve the performance of our Platform, provide you
+        with a streamlined and personalized experience, and protect your account
+        from fraud by detecting unauthorized access.</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >7(a) How do we use the information we collect? we use your personal
+        data for the following purposes</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >1. To offer the services and functions in a safe and lawful way and to
+        administer your use of the Service.</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >2. To maintain and improve our Service.</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >3. To prevent the illegal use of our Service.</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >4. To detect, prevent and address technical issues.</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >5. To notify you about changes to the Service.</span
+      >
+    </p>
+    <p><span style="color: #202124">6. To verify your identity.</span></p>
+    <p>
+      <span style="color: #202124"
+        >7. To provide customer care and support.</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >8. To monitor the usage of the Service.</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >You do not have to share information about yourself if you do not want
+        to. But if you do not, you may not be able to use some (or any) of our
+        services.</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >8. Transfer and Disclosure of Data: For the purposes mentioned above,
+        we may transfer or disclose your personal data to; Courts, law
+        enforcement authorities, regulators, or attorneys if necessary, to
+        comply with the law or for the establishment, exercise, or defense of
+        legal claims and Anyone you give us permission to share it with.</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >9. Third-party Websites: If a User visits any link on this APP that
+        leads to a third-party website or to a third-party partner&rsquo;s
+        website, the User agrees to and shall comply with the separate and
+        independent privacy policies of such third-party websites. The User
+        understands and acknowledges that the Platform is not responsible for
+        the content or activities of such third-party websites or
+        partners.</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >10. Information disclosure to third parties: The Platform does not
+        sell, trade, or otherwise transfer information or allow any other party
+        to collect or use any information from our Platform, save and except
+        such information is disclosed in furtherance of the objectives of this
+        platform to the Platform's affiliates ( trusted third parties who help
+        the Platform operate the Platform's websites, manage the Platform's
+        business or provide services to Users) and under conditions of
+        confidentiality.</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >11. Amendment of this Agreement. The Platform reserves the right to
+        modify this Agreement at any time. The Platform will inform the Users of
+        the amendments made to the Privacy Policy by releasing updates thereof,
+        publishing the effective date of new versions thereof, and highlighting
+        the amendments thereto. Sometimes, but not always, the Platform may
+        issue a notice to Users to inform them of any amendments made to the
+        Privacy Policy. Users shall regularly review the Privacy Policy and
+        focus on amendments thereto if any; and if the Users do not agree to any
+        such amendments, the Users shall promptly stop accessing this Website.
+        Whenever an updated version of this Privacy Policy is released, the
+        User&rsquo;s continued access to and use of this Website shall
+        demonstrate the User&rsquo;s agreement to the updated version of the
+        Privacy Policy.</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >12. Duration for Keeping Personal Information: We will hold your
+        personal information on </span
+      ><strong>PI2P</strong
+      ><span style="color: #202124">
+        systems for as long as is necessary to fulfill the purpose for which it
+        was collected or to comply with legal, regulatory or internal policy
+        requirements.</span
+      >
+    </p>
+    <p>
+      <span style="color: #202124"
+        >13. Communication with Us. Hearing from you remains our greatest
+        pleasure. All User comments or feedback should be sent to the following
+        email address: </span
+      >Support@pi2p.io<span style="color: #202124">
+        This is the only valid and official email address through which the
+        Platform communicates with Users. If a User uses any other means of
+        communication to contact the Platform, the Platform shall not be obliged
+        to reply and the Platform will not be held liable in any manner
+        whatsoever.</span
+      >
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
